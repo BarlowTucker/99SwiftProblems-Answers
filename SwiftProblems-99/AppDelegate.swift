c6d3e72721c649337ff413625d37f75a20f2b6a5
Barlow Tucker <barlowtucker@gmail.com>
2017-06-23 14:55:28 -0600

    Initial Commit

---
+++ SwiftProblems-99/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwiftProblems-99
+//
+//  Created by Barlow Tucker on 6/23/17.
+//  Copyright © 2017 Barlow Tucker. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SwiftProblems-99/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwiftProblems-99
+//
+//  Created by Barlow Tucker on 6/23/17.
+//  Copyright © 2017 Barlow Tucker. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
